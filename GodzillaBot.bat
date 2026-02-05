135ff2311a73ffefdb4da3f3b4d731434764c422
Saura13 <100842663+Saura13@users.noreply.github.com>
2026-02-06 00:27:11 +0100

    Add files via upload

---
+++ GodzillaBot.bat
@@ -0,0 +1,7 @@
+@echo off
+title Lair of GodzillaBot
+echo ---------------------------------------------------
+echo    DESPERTANDO A GODZILLA... POR FAVOR ESPERE
+echo ---------------------------------------------------
+cd /d "%~dp0"
+python -m streamlit run app.py
